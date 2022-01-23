c733e85cd16d35d6def5876820bffb07bc229247
Aliif Arief <aliif.arief.m@gmail.com>
2022-01-23 14:28:52 +0700

    add: comparation

---
+++ 07_comparation/co.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    
+    int a = 3;
+    int b = 3;
+
+    bool result1, result2, result3, result4, result5, result6;
+
+    result1 = a == b;
+    result2 = a != b;
+    result3 = a < b;
+    result4 = a > b;
+    result5 = a <= b;
+    result6 = a >= b;
+
+
+    cout << result1 << endl;
+    cout << result2 << endl;
+    cout << result3 << endl;
+    cout << result4 << endl;
+    cout << result5 << endl;
+    cout << result6 << endl;
+
+    /* 
+    0 = false
+    1 = true
+    */
+}
