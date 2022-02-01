08dee437b2730cf9ef20b7ad3bbc6642a2753a24
Aliif Arief <aliif.arief.m@gmail.com>
2022-02-02 06:37:36 +0700

    add: loop methods

--- 12_loop/cf.cpp
+++ 12_loop/cf.cpp
@@ -3,5 +3,33 @@
 using namespace std;
 
 int main() {
+    for (int i = 0; i <= 10; i++)
+    {
+        if (i == 5)
+        {
+            //break;
+            continue;
+        }
+
+        cout << i << endl;
+        
+    }
+
+
+    cout << "\n\n";
+
+    int j = 0;
+
+    while (j <= 10)
+    {
+        j++;
+        cout << j << endl;
+
+        if (j == 5)
+        {
+            break;
+        }
+        
+    }
     
 }
