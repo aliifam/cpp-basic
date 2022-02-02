d33f2d081e744c25e4f3b3b0ed924e701480fff2
Aliif Arief <aliif.arief.m@gmail.com>
2022-02-02 21:49:57 +0700

    add:function

---
+++ 13_function/introf.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    int a;
+    cout << "masukkan angka = ";
+    cin >> a;
+
+    double b = sqrt(a);
+
+    cout << "akar dari " << a << " = " << b; 
+}
