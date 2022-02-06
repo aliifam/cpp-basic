d5ae653b8b3bb9615c8e30578ee98022e7622491
Aliif Arief <aliif.arief.m@gmail.com>
2022-02-06 15:20:39 +0700

    add: function reference

--- 14_pointer/funcref.cpp
+++ 14_pointer/funcref.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+void fungsi(int &b){
+    b = 10;
+    cout << "nilai dari b = " << b << endl;
+    cout << "alamat dari b = " << &b << endl;
+}
+
+void kuadrat(int &valref) {
+    valref = valref * valref;
+}
+
 int main() {
-    
+    int a = 5;
+
+    cout << "nilai dari a = " << a << endl;
+    cout << "alamat dari a = " << &a << endl;
+
+    cout << endl;
+
+    fungsi(a);
+
+     cout << "nilai dari a = " << a << endl;
+
+    kuadrat(a);
+
+    cout << "kuadrat dari a = " << a << endl;
 }
